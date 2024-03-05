ae4b6693d86666055f0b0967784e84db07e4792c
Lance Hargrave <launcechronicles@gmail.com>
2024-03-05 08:19:43 -0600

    Login now redirects to dashboard

--- budget-buddy/django_project/urls.py
+++ budget-buddy/django_project/urls.py
@@ -6,13 +6,14 @@ from django.views.generic import RedirectView
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path("", RedirectView.as_view(url='/accounts/login/', permanent=False)),
+    path("", RedirectView.as_view(url='/dashboard/', permanent=False)),
     path("admin/", admin.site.urls),
     path("accounts/", include("users.urls")),
     path("accounts/", include("user_profile.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
     path("dashboard/", include("user_dashboard.urls")),
-    path("logout/", auth_views.LogoutView.as_view(), name='logout'),  # Add this line for logout
+    path("accounts/login/", auth_views.LoginView.as_view(), name='login'),
+    path("logout/", auth_views.LogoutView.as_view(), name='logout'),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
