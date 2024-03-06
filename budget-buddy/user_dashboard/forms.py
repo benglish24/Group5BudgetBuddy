bb7882aa2f6dee272408d5a80e95b64981b48791
k-murda <vitamink69420@gmail.com>
2024-03-06 16:32:42 -0600

    created transaction table and CRUD operations for it

--- budget-buddy/user_dashboard/forms.py
+++ budget-buddy/user_dashboard/forms.py
@@ -1,7 +1,18 @@
+from django.forms import ModelForm
+from .models import UserDashboard, Transaction
 from django import forms
-from .models import UserDashboard
 
-class UserDashboardForm(forms.ModelForm):
+class UserDashboardForm(ModelForm):
     class Meta:
         model = UserDashboard
         fields = ['salary', 'saving_percentage', 'fixed_percentage', 'spending']
+
+class TransactionForm(ModelForm):
+    class Meta:
+        model = Transaction
+        fields = ['date_of', 'description', 'amount', 'category']
+
+        # makes date input in form an actual date picker
+        widgets = {
+            'date_of' : forms.widgets.DateInput(attrs={'type' : 'date'})
+        }
--- budget-buddy/user_dashboard/views.py
+++ budget-buddy/user_dashboard/views.py
@@ -1,12 +1,65 @@
-from django.shortcuts import render
-from .models import UserDashboard
+from django.shortcuts import render, redirect
+from .models import UserDashboard, Transaction
 from .decorators import authenticated_user
+
+from .forms import TransactionForm
+from django.http import HttpResponse
 
 @authenticated_user
 def user_dashboard(request):
     # Get or create the UserDashboard object for the current user
-    user_dashboard, created = UserDashboard.objects.get_or_create(custom_user=request.user)
+    # user_dashboard, created = UserDashboard.objects.get_or_create(custom_user=request.user)
+    user_dashboard = UserDashboard.objects.get(custom_user=request.user)
+    transactions = Transaction.objects.filter(user=request.user)
 
     # If the object was just created, it means it's empty and you might want to set default values here
 
-    return render(request, 'user_dash.html', {'user_dashboard': user_dashboard})
+    return render(request, 'user_dash.html', {'user_dashboard': user_dashboard, 'transactions' : transactions})
+
+
+@authenticated_user
+def add_transaction(request):
+    if request.method == 'POST':
+        form = TransactionForm(request.POST)
+        if form.is_valid():
+            transaction = form.save(commit=False)
+            transaction.user = request.user
+            transaction.save()
+            return redirect('/')
+
+    form = TransactionForm
+    context = {'form' : form }
+    return render(request, 'transaction_form.html', context)
+
+
+@authenticated_user
+def update_transaction(request, pk):
+    transaction = Transaction.objects.get(id=pk)
+    form = TransactionForm(instance=transaction)
+
+    if request.user != transaction.user:
+        return HttpResponse("You should not be here.")
+
+    if request.method == 'POST':
+        form = TransactionForm(request.POST, instance=transaction)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+    context = {'form' : form }
+    return render(request, 'transaction_form.html', context)
+
+
+@authenticated_user
+def delete_transaction(request, pk):
+    transaction = Transaction.objects.get(id=pk)
+
+    if request.user != transaction.user:
+        return HttpResponse("You should not be here.")
+
+    if request.method == 'POST':
+        transaction.delete()
+        return redirect('/')
+
+    context = {"transaction" : transaction}
+    return render(request, 'delete.html', context)
