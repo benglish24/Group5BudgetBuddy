368c76628a1b00470857b24796dd1402babf440f
Cameron <cameronpmatherne@gmail.com>
2024-03-20 12:57:57 -0500

    added commas for number values

--- budget-buddy/django_project/settings.py
+++ budget-buddy/django_project/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django.contrib.humanize",
     # third-party apps
     "django_bootstrap5",
     "fontawesomefree",
